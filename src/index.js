4c0538671d79a34b575b62b2ab31577aa743558b
Shaun <shaun@thenetninja.co.uk>
2021-10-26 16:50:06 +0100

    lesson-4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmXgb_58lO7aK_ujN37pGlNxzWGEU0YpI",
@@ -11,3 +14,23 @@ const firebaseConfig = {
 
 // init firebase
 initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+  .then(snapshot => {
+    // console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach(doc => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
