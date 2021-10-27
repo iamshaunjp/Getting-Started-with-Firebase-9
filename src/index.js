4fc162f56eb5bdb2d007274740957294f2c64f06
Shaun <shaun@thenetninja.co.uk>
2021-10-27 08:56:05 +0100

    lesson-13

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, signOut
 } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -104,6 +105,35 @@ signupForm.addEventListener('submit', (e) => {
     .then(cred => {
       console.log('user created:', cred.user)
       signupForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
     })
     .catch(err => {
       console.log(err.message)
