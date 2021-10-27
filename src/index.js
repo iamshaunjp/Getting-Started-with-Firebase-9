b7fce51a25108e64f5e42d7d981a2464e7eba380
Shaun <shaun@thenetninja.co.uk>
2021-10-27 08:51:34 +0100

    lesson-12

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import {
   updateDoc
 } from 'firebase/firestore'
 import {
-  getAuth
+  getAuth,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -89,4 +90,22 @@ updateForm.addEventListener('submit', (e) => {
   .then(() => {
     updateForm.reset()
   })
+})
+
+// signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = signupForm.email.value
+  const password = signupForm.password.value
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user created:', cred.user)
+      signupForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 })
