dec06606907f53d209f49d4c5e7248c8740dcbb4
Shaun <9838872+iamshaunjp@users.noreply.github.com>
2021-10-27 17:00:02 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  devtool: 'eval-source-map',
-  watch: true,
+  watch: true
 }
